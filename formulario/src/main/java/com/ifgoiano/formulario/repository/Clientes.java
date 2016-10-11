2da939389ee25f046c0abd1e7084c08b31d756ff
ShanksRuivo <matheusnevesdecarvalho@gmail.com>
2016-10-11 20:30:33 -0300

    Exclusão
    Teste de Exclusão

--- formulario/src/main/java/com/ifgoiano/formulario/repository/Clientes.java
+++
@@ -1,9 +0,0 @@
-package com.ifgoiano.formulario.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ifgoiano.formulario.model.Cliente;
-
-public interface Clientes extends JpaRepository<Cliente, Long>{
-
-}
