7beab055b4002b8bd8110d4e74785b11b54b36b1
ShanksRuivo <matheusnevesdecarvalho@gmail.com>
2016-10-17 20:51:22 -0200

    correção

--- sistemaSupermecado/src/main/java/com/ifgoiano/supermecado/controller/ProdutoController.java
+++ sistemaSupermecado/src/main/java/com/ifgoiano/supermecado/controller/ProdutoController.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.gargoylesoftware.htmlunit.javascript.host.fetch.Request;
+
 import com.ifgoiano.supermecado.model.Produto;
+import com.ifgoiano.supermecado.repository.Produtos;
 
 @Controller
-@RequestMapping("/Produtos")
-
+@RequestMapping("/produtos")
 public class ProdutoController {
 
 	private static final String CADASTRO_PRODUTO = "CadastroProduto";
 	
 	@Autowired
-	private Produto produto;
+	private Produtos produto;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo(){
 		ModelAndView mv = new ModelAndView(CADASTRO_PRODUTO);
 		return mv;
 	}
-	@RequestMapping(method = RequestMethod.POST)
+	/*@RequestMapping(method = RequestMethod.POST)
 	public String salvar(){
 		return "cadastro";
-	}
+	}*/
 }
