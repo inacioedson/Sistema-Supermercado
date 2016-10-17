f51644dfea7df292a332c34bb5ec7154d3a9deb0
ShanksRuivo <matheusnevesdecarvalho@gmail.com>
2016-10-17 19:37:27 -0200

    Criação do model Categoria

---
+++ sistemaSupermecado/src/main/java/com/ifgoiano/supermecado/model/Categoria.java
@@ -0,0 +1,72 @@
+package com.ifgoiano.supermecado.model;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="pk_categoria")
+	private long id;
+	
+	
+	private String nome;
+	
+	private String descricao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+}
