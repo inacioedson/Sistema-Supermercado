c0bf1653b497b2d3507ac5b7854761abb8b4d179
ShanksRuivo <matheusnevesdecarvalho@gmail.com>
2016-10-17 19:37:43 -0200

    Criação do controller Categoria

---
+++ sistemaSupermecado/src/main/java/com/ifgoiano/supermecado/controller/CategoriaController.java
@@ -0,0 +1,37 @@
+package com.ifgoiano.supermecado.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ifgoiano.supermecado.model.Categoria;
+import com.ifgoiano.supermecado.repository.Categorias;
+
+
+
+@Controller
+@RequestMapping("/categorias")
+public class CategoriaController {
+	
+	@Autowired
+	private Categorias categorias;
+	
+	@RequestMapping("/novo")
+	public String novo(){
+		
+		return"CadastroCategoria";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView salvar(Categoria catego){
+		
+		categorias.save(catego);
+		ModelAndView mv = new ModelAndView("CadastroCategoria");	
+		mv.addObject("mensagem", "A operação foi completa com sucesso");
+		return mv;
+		
+	}
+
+}
