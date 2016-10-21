f18a19d17e1a62e4e110441a38315b30689e1591
BrunnoHCInacio <brunnoh11@gmail.com>
2016-10-21 16:35:32 -0200

    Alteração do arquivo validacao.js e validação da pagina de categorias

--- sistemaSupermecado/src/main/resources/static/js/validacao.js
+++ sistemaSupermecado/src/main/resources/static/js/validacao.js
@@ -1,77 +1,117 @@
-function validaForm() {
-	var form = document.forms['formProduto'];
-	var formOk = true;
-	
-	
+function validaFormCadProduto() {
+	var formProduto = document.forms['formProduto'];
+	var formProdutoOk = true;
 
-	if (!validaCodigoBarras(form.codigoBarras.value)) {
-		console.log('codigo errado');
-	formOk = false;
-		}
-	
-	if(!validaNome(form.nome.value)){
-		console.log('nome errado');
-		formOk = false;
+	if (!validaCodigoBarras(formProduto.codigoBarras.value)) {
+		
+		formProdutoOk = false;
 	}
-	
-	if(!validaValorUnitario(form.valorUnitario.value)){
-		console.log('Valor unitario errado');
-		formOk = false;
+
+	if (!validaNome(formProduto.nome.value)) {
+		
+		formProdutoOk = false;
 	}
-	
-	if(!validaQtdEstoque(form.qtdEstoque.value)){
-		console.log('Qtd estoque errado');
-		formOk = false;
+
+	if (!validaValorUnitario(formProduto.valorUnitario.value)) {
+		
+		formProdutoOk = false;
 	}
-	
-	if(!validaQtdMinimaEstoque(form.qtdMinimo.value)){
-		console.log('Quantidade mínima errada');
-		formOk = false;
+
+	if (!validaQtdEstoque(formProduto.qtdEstoque.value)) {
+		
+		formProdutoOk = false;
 	}
-	
-	if(!validaCategoria()){
-		console.log('Categoria errado');
-		formOk = false;
+
+	if (!validaQtdMinimaEstoque(formProduto.qtdMinimo.value)) {
+		
+		formProdutoOk = false;
 	}
-	
-	
-	if(formOk) return true;
-	else return false;
-	
-	
+
+	if (!validaCategoria()) {
+		
+		formProdutoOk = false;
+	}
+
+	if (formProdutoOk)
+		return true;
+	else
+		return false;
+
+	function validaCodigoBarras(codigoBarras) {
+		if (codigoBarras == "" | codigoBarras == undefined)
+			return false;
+		else
+			return true;
+	}
+
+	function validaNome(nome) {
+		if (nome == "" | nome == undefined)
+			return false;
+		else
+			return true;
+	}
+
+	function validaValorUnitario(valorUnitario) {
+		if (valorUnitario == "" | valorUnitario == undefined)
+			return false;
+		else
+			return true;
+	}
+
+	function validaQtdEstoque(qtdEstoque) {
+		if (qtdEstoque == "" | qtdEstoque == undefined)
+			return false;
+		else
+			return true;
+	}
+
+	function validaQtdMinimaEstoque(qtdMinimaEstoque) {
+		if (qtdMinimaEstoque == "" | qtdMinimaEstoque == undefined)
+			return false;
+		else
+			return true;
+	}
 }
 
-function validaCodigoBarras(codigoBarras) {
-	if (codigoBarras == "" | codigoBarras == undefined)	return false;
+function validaCategoria() {
+	// cadastroProdutoSelect();
+	var categoria = document.forms['formProduto'].idCategoria;
+	
+	var categoriaIndex = categoria.selectedIndex;
+	if (categoriaIndex == 0 | categoriaIndex == undefined)
+		return false;
 	else
 		return true;
 }
 
-function validaNome(nome){
-	if(nome=="" | nome == undefined) return false
-	else return true;
-}
+function validaFormCadCategoria() {
 
-function validaValorUnitario(valorUnitario){
-	if(valorUnitario == "" | valorUnitario == undefined) return false;
-	else return true;
-}
+	var formCategoria = document.forms['formCategoria'];
+	var formCategoriaOK = true;
 
-function validaQtdEstoque(qtdEstoque){
-	if(qtdEstoque == "" | qtdEstoque == undefined) return false;
-	else return true;
-}
+	if (!validaNome(formCategoria.nome.value)) {
+		console.log('Nome da categoria errado');
+		formCategoriaOK = false;
+	}
+	if (!validaDescricao(formCategoria.descricao.value)) {
+		console.log('Descrição errado');
+		formCategoriaOK = false;
+	}
+	if (formCategoriaOK)
+		return true;
+	else
+		return false;
 
-function validaQtdMinimaEstoque(qtdMinimaEstoque){
-	if(qtdMinimaEstoque == "" | qtdMinimaEstoque == undefined) return false;
-	else return true;
-}
-
-function validaCategoria(){
-	//cadastroProdutoSelect();
-	var categoria = document.forms['formProduto'].idCategoria;
-	alert(categoria.value);
-	var categoriaIndex = categoria.selectedIndex;
-	if(categoriaIndex == 0 | categoriaIndex == undefined) return false;
-	else return true;
+	function validarNome(nome) {
+		if (nome == "" | nome == undefined)
+			return false;
+		else
+			return true;
+	}
+	function validaDescricao(descricao) {
+		if (descricao == "" | descricao == undefined)
+			return false;
+		else
+			return true;
+	}
 }
