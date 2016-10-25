14936fe605ac3ce994f51cf939de98e248957b9c
ShanksRuivo <matheusnevesdecarvalho@gmail.com>
2016-10-25 20:19:40 -0200

    Novo Template

---
+++ sistemaSupermecadoWeb/src/main/java/com/ifgoiano/supermecado/SistemaSupermecadoWebApplication.java
@@ -0,0 +1,12 @@
+package com.ifgoiano.supermecado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSupermecadoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSupermecadoWebApplication.class, args);
+	}
+}
---
+++ sistemaSupermecadoWeb/src/main/java/com/ifgoiano/supermecado/model/Produto.java
@@ -0,0 +1,5 @@
+package com.ifgoiano.supermecado.model;
+
+public class Produto {
+
+}
