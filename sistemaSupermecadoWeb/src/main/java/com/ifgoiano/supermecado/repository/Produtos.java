f5960a7441ae9bf65340239ed09a4da0ea8e850d
Lucas Gonçalves <lucaslucasfabio@hotmail.com>
2016-10-27 21:56:02 -0200

    Comit

--- sistemaSupermecadoWeb/src/main/java/com/ifgoiano/supermecado/repository/Produtos.java
+++ sistemaSupermecadoWeb/src/main/java/com/ifgoiano/supermecado/repository/Produtos.java
@@ -1,6 +1,15 @@
 package com.ifgoiano.supermecado.repository;
 
-public class Produtos {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.ifgoiano.supermecado.model.Produto;
+
+public interface Produtos extends JpaRepository<Produto, Long> {
+	
+	
+/*	 @Query(value = "SELECT * FROM USERS WHERE EMAIL_ADDRESS = ?0", nativeQuery = true)
+	  User findByEmailAddress(String emailAddress);*/
+	@Query(value="SELECT p.codigo, p.nome FROM produto p", nativeQuery=true)
+	Produto consulta(); 
 }
- 
