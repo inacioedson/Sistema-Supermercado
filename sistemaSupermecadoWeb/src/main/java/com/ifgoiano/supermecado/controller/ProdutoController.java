6e768630ccd2cd0b1a499e5f743ac07d4aa29a81
Lucas Gonçalves <lucaslucasfabio@hotmail.com>
2016-11-08 22:43:05 -0200

    Modificacoes para funcionamento

--- sistemaSupermecadoWeb/src/main/java/com/ifgoiano/supermecado/controller/ProdutoController.java
+++ sistemaSupermecadoWeb/src/main/java/com/ifgoiano/supermecado/controller/ProdutoController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.ifgoiano.supermecado.controller;
 
 import java.util.Arrays;
@@ -38,7 +38,7 @@ import com.ifgoiano.supermecado.repository.Produtos;
 @RequestMapping("/produtos")
 public class ProdutoController {
 
-	private static final String CADASTRO_VIEW= "produto/CadastroProduto";
+	private static final String CADASTRO_VIEW = "produto/CadastroProduto";
 
 	@Autowired
 
@@ -50,7 +50,7 @@ public class ProdutoController {
 
 	@RequestMapping("/novo")
 
-	public ModelAndView novo(){
+	public ModelAndView novo() {
 
 		List<Categoria> todosCategoria = categorias.findAll();
 
@@ -62,18 +62,18 @@ public class ProdutoController {
 
 	}
 
-	@RequestMapping(method=RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST)
 
-	public String salvar(@Validated Produto prod,Errors errors,RedirectAttributes attributes){
+	public String salvar(@Validated Produto prod, Errors errors, RedirectAttributes attributes) {
 
-		if(errors.hasErrors()){
+		if (errors.hasErrors()) {
 
 			return CADASTRO_VIEW;
 
 		}
 
 		produtos.save(prod);
-		attributes.addFlashAttribute("mensagem","Produto salvo com sucesso!");
+		attributes.addFlashAttribute("mensagem", "Produto salvo com sucesso!");
 
 		return "redirect:/produtos/novo";
 
@@ -81,7 +81,7 @@ public class ProdutoController {
 
 	@RequestMapping
 
-	public ModelAndView pesquisar(){
+	public ModelAndView pesquisar() {
 
 		List<Produto> todosProduto = produtos.findAll();
 
@@ -94,144 +94,24 @@ public class ProdutoController {
 	}
 
 	@RequestMapping("{id}")
-public ModelAndView edicao(@PathVariable("id") Produto prod){
-
-	List<Categoria> todosCategoria = categorias.findAll();
-
-	ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
-
-	mv.addObject("categorias", todosCategoria);
-
-	mv.addObject(prod);
-
-	return mv;
-	}
-	
-	@RequestMapping(value="{id}", method = RequestMethod.DELETE)
-	public String excluir(@PathVariable Long id, RedirectAttributes attributes) {
-		produtos.delete(id);
-		
-		attributes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
-		return "redirect:/produtos";
-	}
-	}
-
-=======
-package com.ifgoiano.supermecado.controller;
-
-import java.util.Arrays;
-
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.stereotype.Controller;
-
-import org.springframework.validation.Errors;
-
-import org.springframework.validation.annotation.Validated;
-
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import org.springframework.web.bind.annotation.PathVariable;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.servlet.ModelAndView;
-
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.ifgoiano.supermecado.model.Categoria;
-
-import com.ifgoiano.supermecado.model.Produto;
-
-import com.ifgoiano.supermecado.repository.Categorias;
-
-import com.ifgoiano.supermecado.repository.Produtos;
-
-@Controller
-
-@RequestMapping("/produtos")
-public class ProdutoController {
-
-	private static final String CADASTRO_VIEW= "produto/CadastroProduto";
-
-	@Autowired
-
-	private Produtos produtos;
-
-	@Autowired
-
-	private Categorias categorias;
-
-	@RequestMapping("/novo")
-
-	public ModelAndView novo(){
+	public ModelAndView edicao(@PathVariable("id") Produto prod) {
 
 		List<Categoria> todosCategoria = categorias.findAll();
 
 		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 
 		mv.addObject("categorias", todosCategoria);
-		mv.addObject(new Produto());
-		return mv;
 
-	}
-
-	@RequestMapping(method=RequestMethod.POST)
-
-	public String salvar(@Validated Produto prod,Errors errors,RedirectAttributes attributes){
-
-		if(errors.hasErrors()){
-
-			return CADASTRO_VIEW;
-
-		}
-
-		produtos.save(prod);
-		attributes.addFlashAttribute("mensagem","Produto salvo com sucesso!");
-
-		return "redirect:/produtos/novo";
-
-	}
-
-	@RequestMapping
-
-	public ModelAndView pesquisar(){
-
-		List<Produto> todosProduto = produtos.findAll();
-
-		ModelAndView mv = new ModelAndView("produto/PesquisarProduto");
-
-		mv.addObject("produtos", todosProduto);
+		mv.addObject(prod);
 
 		return mv;
-
 	}
 
-	@RequestMapping("{id}")
-public ModelAndView edicao(@PathVariable("id") Produto prod){
-
-	List<Categoria> todosCategoria = categorias.findAll();
-
-	ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
-
-	mv.addObject("categorias", todosCategoria);
-
-	mv.addObject(prod);
-
-	return mv;
-	}
-	
-	@RequestMapping(value="{id}", method = RequestMethod.DELETE)
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
 	public String excluir(@PathVariable Long id, RedirectAttributes attributes) {
 		produtos.delete(id);
-		
+
 		attributes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
 		return "redirect:/produtos";
 	}
-	}
-
->>>>>>> master
+}
